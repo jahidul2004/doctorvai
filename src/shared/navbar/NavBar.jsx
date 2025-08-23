a399157e60eff25dd4bbe9a86fffbcd8bd78c313
Jahidul Islam Jihad <islamjahiduljihad@gmail.com>
2025-08-23 23:43:35 +0600

    Navbar updated.

--- src/shared/navbar/NavBar.jsx
+++ src/shared/navbar/NavBar.jsx
@@ -53,15 +53,55 @@ const NavBar = () => {
             </li>
         </>
     );
+
     return (
-        <div className="navbar bg-base-100 shadow-sm px-1 md:px-10 lg:px-16">
-            <div className="flex-1">
-                <div>
-                    <img className="w-[180px]" src={logo} alt="" />
+        <div className="drawer">
+            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col">
+                {/* Navbar */}
+                <div className="w-full navbar bg-base-100 shadow-sm px-1 md:px-10 lg:px-16">
+                    <div className="flex-1">
+                        <img className="w-[180px]" src={logo} alt="" />
+                    </div>
+                    <div className="flex-none hidden lg:block">
+                        <ul className="menu menu-horizontal px-1">{links}</ul>
+                    </div>
+                    <div className="flex-none lg:hidden border rounded-md border-[#06aed4] text-[#06aed4]">
+                        <label
+                            htmlFor="my-drawer-3"
+                            aria-label="open sidebar"
+                            className="btn btn-square btn-ghost"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                className="inline-block w-6 h-6 stroke-current"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                ></path>
+                            </svg>
+                        </label>
+                    </div>
                 </div>
             </div>
-            <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">{links}</ul>
+            <div className="drawer-side">
+                <label
+                    htmlFor="my-drawer-3"
+                    aria-label="close sidebar"
+                    className="drawer-overlay"
+                ></label>
+                <ul className="menu p-4 w-80 min-h-full bg-base-200">
+                    {/* Sidebar content */}
+                    <div className="flex justify-center my-4">
+                        <img className="w-[180px]" src={logo} alt="" />
+                    </div>
+                    {links}
+                </ul>
             </div>
         </div>
     );
