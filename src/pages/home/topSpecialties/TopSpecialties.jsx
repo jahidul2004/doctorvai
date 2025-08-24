f1417a32ce7ec9e5450bdec2eb1c98c70e17b66d
Jahidul Islam Jihad <islamjahiduljihad@gmail.com>
2025-08-24 11:42:58 +0600

    Top specialties section added.

--- src/pages/home/topSpecialties/TopSpecialties.jsx
+++ src/pages/home/topSpecialties/TopSpecialties.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import SectionHead from "../../../components/sectionHead/SectionHead";
 import SpecialtyCard from "../../../components/specialtyCard/SpecialtyCard";
+
 import neurologyIcon from "../../../assets/specialtyIcons/neurologyIcon.svg";
+import neurologyImage from "../../../assets/specialtyIcons/neurologyImage.jpg";
+
+import orthopedicsIcon from "../../../assets/specialtyIcons/orthopedicsIcon.svg";
+import orthopedicsImage from "../../../assets/specialtyIcons/orthopedicsImage.jpg";
+
+import cardiologyIcon from "../../../assets/specialtyIcons/cardiologyIcon.svg";
+import cardiologyImage from "../../../assets/specialtyIcons/cardiologyImage.jpg";
+
+import pediatricsIcon from "../../../assets/specialtyIcons/pediatricsIcon.svg";
+import pediatricsImage from "../../../assets/specialtyIcons/pediatricsImage.jpg";
+
+import psychiatryIcon from "../../../assets/specialtyIcons/psychiatryIcon.svg";
+import psychiatryImage from "../../../assets/specialtyIcons/psychiatryImage.jpg";
+
+import endocrinologyIcon from "../../../assets/specialtyIcons/endocrinologyIcon.svg";
+import endocrinologyImage from "../../../assets/specialtyIcons/endocrinologyImage.jpg";
+
+import urologyIcon from "../../../assets/specialtyIcons/urologyIcon.svg";
+import urologyImage from "../../../assets/specialtyIcons/urologyImage.jpg";
+
+import pulmonologyIcon from "../../../assets/specialtyIcons/pulmonologyIcon.svg";
+import pulmonologyImage from "../../../assets/specialtyIcons/pulmonologyImage.jpg";
 
 const TopSpecialties = () => {
     return (
@@ -16,11 +39,60 @@ const TopSpecialties = () => {
             {/* Specialty card */}
             <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4 md:gap-6">
                 <SpecialtyCard
-                    path="/Cardiology"
-                    image="https://i.ibb.co.com/JRRmGLNW/photo-1618939304347-e91b1f33d2ab.jpg"
+                    path="/Neurology"
+                    image={neurologyImage}
                     icon={neurologyIcon}
                     title={"Neurology"}
                     doctors={10}
+                />
+                <SpecialtyCard
+                    path="/Orthopedics"
+                    image={orthopedicsImage}
+                    icon={orthopedicsIcon}
+                    title={"Orthopedics"}
+                    doctors={12}
+                />
+                <SpecialtyCard
+                    path="/Cardiology"
+                    image={cardiologyImage}
+                    icon={cardiologyIcon}
+                    title={"Cardiology"}
+                    doctors={18}
+                />
+                <SpecialtyCard
+                    path="/Pediatrics"
+                    image={pediatricsImage}
+                    icon={pediatricsIcon}
+                    title={"Pediatrics"}
+                    doctors={14}
+                />
+                <SpecialtyCard
+                    path="/Psychiatry"
+                    image={psychiatryImage}
+                    icon={psychiatryIcon}
+                    title={"Psychiatry"}
+                    doctors={5}
+                />
+                <SpecialtyCard
+                    path="/Endocrinology"
+                    image={endocrinologyImage}
+                    icon={endocrinologyIcon}
+                    title={"Endocrinology"}
+                    doctors={11}
+                />
+                <SpecialtyCard
+                    path="/Urology"
+                    image={urologyImage}
+                    icon={urologyIcon}
+                    title={"Urology"}
+                    doctors={11}
+                />
+                <SpecialtyCard
+                    path="/Pulmonology"
+                    image={pulmonologyImage}
+                    icon={pulmonologyIcon}
+                    title={"Pulmonology"}
+                    doctors={14}
                 />
             </div>
             {/* Specialty card end */}
